6b52be729020305781b2f1cd6a5b9bd0f1915234
foo <leovinamf05@gmail.com>
2021-12-02 23:17:52 +1000

    color wheel

--- js/script.js
+++ js/script.js
@@ -482,7 +482,7 @@ function setAllData() {
           datasets: [
               {
                   data: _chartData,
-                  backgroundColor: ['#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c','#fdbf6f','#ff7f00','#cab2d6','#6a3d9a','#ffff99','#b15928'],
+                  backgroundColor: ['#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c','#fdbf6f','#ff7f00','#cab2d6','#6a3d9a','#ffff99','#b15928','#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5','#d9d9d9','#bc80bd','#ccebc5','#ffed6f'],
                   borderWidth: 0,
               }
           ]
