1c3b261f9288328b0ba46b8fd6cc83a2ff9a60ec
foo <leovinamf05@gmail.com>
2021-12-24 22:28:27 +1000

    fix https

--- js/stats.js
+++ js/stats.js
@@ -44,7 +44,7 @@ function loadData(i, url) {
   let trimRonin = i.eth.replace("ronin:", "");
 
   $.ajax({
-  url: 'http://game-api.skymavis.com/game-api/clients/'+xRonin+'/'+url,
+  url: 'https://game-api.skymavis.com/game-api/clients/'+xRonin+'/'+url,
   type: 'GET',
   beforeSend: function (xhr) {
       xhr.setRequestHeader('Authorization', 'Bearer '+i.key);
