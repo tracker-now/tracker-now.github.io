f1b7a65f632ad21bb0babb357747c2948550b994
foo <leovinamf05@gmail.com>
2022-01-10 23:25:54 +1000

    battlelogs

--- js/battlelogs.js
+++ js/battlelogs.js
@@ -1,28 +1,21 @@
-setTimeout(function(){ $('#loading').hide(); }, 2000);
+setTimeout(function(){ $('#loading').hide(); }, 1000);
 
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
+$('#saveBtn').click(() => {
+    let res = JSON.parse($('#sData').val());
 
-if(urlParams.get('ronin')) {
-  $.ajax({
-    url: 'https://tracking.skymavis.com/battle-history?type=pvp&player_id='+urlParams.get('ronin'),
-    success: function(res) {
+    res.battles.forEach((item, i) => {
+      console.log(item);
+      $('#detail').append(`<tr>
+        <td class="axie-img"><a href="https://marketplace.axieinfinity.com/axie/${item.first_team_fighters[0]}" target="_blank"><img src="https://storage.googleapis.com/assets.axieinfinity.com/axies/${item.first_team_fighters[0]}/axie/axie-full-transparent.png"/></a></td>
+        <td class="axie-img"><a href="https://marketplace.axieinfinity.com/axie/${item.first_team_fighters[1]}" target="_blank"><img src="https://storage.googleapis.com/assets.axieinfinity.com/axies/${item.first_team_fighters[1]}/axie/axie-full-transparent.png"/></a></td>
+        <td class="axie-img"><a href="https://marketplace.axieinfinity.com/axie/${item.first_team_fighters[2]}" target="_blank"><img src="https://storage.googleapis.com/assets.axieinfinity.com/axies/${item.first_team_fighters[2]}/axie/axie-full-transparent.png"/></a></td>
+        <td>VS</td>
+        <td class="axie-img"><a href="https://marketplace.axieinfinity.com/axie/${item.second_team_fighters[0]}" target="_blank"><img src="https://storage.googleapis.com/assets.axieinfinity.com/axies/${item.second_team_fighters[0]}/axie/axie-full-transparent.png"/></a></td>
+        <td class="axie-img"><a href="https://marketplace.axieinfinity.com/axie/${item.second_team_fighters[1]}" target="_blank"><img src="https://storage.googleapis.com/assets.axieinfinity.com/axies/${item.second_team_fighters[1]}/axie/axie-full-transparent.png"/></a></td>
+        <td class="axie-img"><a href="https://marketplace.axieinfinity.com/axie/${item.second_team_fighters[2]}" target="_blank"><img src="https://storage.googleapis.com/assets.axieinfinity.com/axies/${item.second_team_fighters[2]}/axie/axie-full-transparent.png"/></a></td>
+        <td><a href="https://cdn.axieinfinity.com/game/deeplink.html?f=rpl&q=${item.battle_uuid}" target="_blank" class="text-primary">${item.eloAndItem[0].result_type}</a></td>
+        <tr>`);
+    });
 
-      res.battles.forEach((item, i) => {
-        console.log(item);
-        $('#detail').append(`<tr>
-          <td class="axie-img"><a href="https://marketplace.axieinfinity.com/axie/${item.first_team_fighters[0]}" target="_blank"><img src="https://storage.googleapis.com/assets.axieinfinity.com/axies/${item.first_team_fighters[0]}/axie/axie-full-transparent.png"/></a></td>
-          <td class="axie-img"><a href="https://marketplace.axieinfinity.com/axie/${item.first_team_fighters[1]}" target="_blank"><img src="https://storage.googleapis.com/assets.axieinfinity.com/axies/${item.first_team_fighters[1]}/axie/axie-full-transparent.png"/></a></td>
-          <td class="axie-img"><a href="https://marketplace.axieinfinity.com/axie/${item.first_team_fighters[2]}" target="_blank"><img src="https://storage.googleapis.com/assets.axieinfinity.com/axies/${item.first_team_fighters[2]}/axie/axie-full-transparent.png"/></a></td>
-          <td>VS</td>
-          <td class="axie-img"><a href="https://marketplace.axieinfinity.com/axie/${item.second_team_fighters[0]}" target="_blank"><img src="https://storage.googleapis.com/assets.axieinfinity.com/axies/${item.second_team_fighters[0]}/axie/axie-full-transparent.png"/></a></td>
-          <td class="axie-img"><a href="https://marketplace.axieinfinity.com/axie/${item.second_team_fighters[1]}" target="_blank"><img src="https://storage.googleapis.com/assets.axieinfinity.com/axies/${item.second_team_fighters[1]}/axie/axie-full-transparent.png"/></a></td>
-          <td class="axie-img"><a href="https://marketplace.axieinfinity.com/axie/${item.second_team_fighters[2]}" target="_blank"><img src="https://storage.googleapis.com/assets.axieinfinity.com/axies/${item.second_team_fighters[2]}/axie/axie-full-transparent.png"/></a></td>
-          <td><a href="https://cdn.axieinfinity.com/game/deeplink.html?f=rpl&q=${item.battle_uuid}" target="_blank" class="text-primary">${item.eloAndItem[0].result_type}</a></td>
-          <tr>`);
-      });
-
-      $('#loading').hide();
-    }
-  });
-}
+    $('#addData').modal('hide');
+});
